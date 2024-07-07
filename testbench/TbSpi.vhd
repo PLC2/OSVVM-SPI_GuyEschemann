77d1e30c494d0e6032918cde70c179a5b575976a
Jim Lewis <jim@synthworks.com>
2024-07-06 20:46:57 -0700

    Replaced TbUtilPkg.Create... with ClockResetPkg.Create...

--- testbench/TbSpi.vhd
+++ testbench/TbSpi.vhd
@@ -81,7 +81,7 @@ begin
     ------------------------------------------------------------
     -- Create Clock
     ------------------------------------------------------------
-    Osvvm.TbUtilPkg.CreateClock(
+    Osvvm.ClockResetPkg.CreateClock(
         Clk    => Clk,
         Period => tperiod_Clk
     );
@@ -89,7 +89,7 @@ begin
     ------------------------------------------------------------
     -- Create Reset
     ------------------------------------------------------------
-    Osvvm.TbUtilPkg.CreateReset(
+    Osvvm.ClockResetPkg.CreateReset(
         Reset       => n_Reset,
         ResetActive => '0',
         Clk         => Clk,
